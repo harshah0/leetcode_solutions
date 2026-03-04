f7990f240956ae7330fd91b171643e34964d9b45
harshah0 <153075051+harshah0@users.noreply.github.com>
2026-03-04 15:19:48 +0530

    Time: 4 ms (50.51%), Space: 21 MB (28.27%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,10 +1,9 @@
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ones=0
-        twos=0
-        for num in nums:
-            ones=(ones^num)&(~twos)
-            twos=(twos^num)&(~ones)
-            
-        return ones
-        
+        d=Counter(nums)
+        for k,v in d.items():
+            if v==1:
+                return k
+        return -1
+       
