258a688e373a47777825568ff22d25531e8bfbf7
harshah0 <153075051+harshah0@users.noreply.github.com>
2026-04-09 21:55:08 +0530

    Time: 22 ms (31.4%), Space: 80.8 MB (74.41%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int num:nums){
+            hs.add(num);
+        }
+        return hs.size()<nums.length;
+    }
+}
